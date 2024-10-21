cd507885e80081b92a3c675bf34e8b253a9c7750
souzaWill <57758087+souzaWill@users.noreply.github.com>
2024-10-21 19:28:49 -0300

    feat: first version of home page

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,7 +1,67 @@
 <template>
-  <HelloWorld />
+  <v-app>
+    <v-app-bar app color="primary" dark>
+      <v-app-bar-nav-icon @click="toggleDrawer"></v-app-bar-nav-icon>
+      <v-toolbar-title>Meu App</v-toolbar-title>
+    </v-app-bar>
+
+    <v-navigation-drawer app v-model="drawer" color="primary lighten-4">
+      <v-list>
+        <v-list-item v-for="(item, index) in items" :key="index" link @click="navigate(item.route)">
+          <v-list-item-title>{{ item.title }}</v-list-item-title>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+
+    <v-main>
+      <v-container fluid>
+        <router-view>
+          <h1 v-if="isAuthenticated">Bem-vindo, {{ userName }}!</h1>
+        </router-view>
+      </v-container>
+    </v-main>
+  </v-app>
 </template>
 
-<script setup>
-  //
+<script>
+import { ref } from 'vue';
+import { useRouter } from 'vue-router';
+import { useAuthStore } from '@/store/auth';
+import { computed } from 'vue';
+
+export default {
+  setup() {
+    const drawer = ref(true);
+    const router = useRouter();
+    const authStore = useAuthStore();
+
+    const isAuthenticated = computed(() => authStore.isAuthenticated);
+    const userName = computed(() => authStore.getUserName);
+
+    const items = [
+      { title: 'Home', route: '/' },
+      { title: 'Students', route: '/students' },
+    ];
+
+    const navigate = (route) => {
+      router.push(route);
+    };
+
+    const toggleDrawer = () => {
+      drawer.value = !drawer.value;
+    };
+
+    return {
+      drawer,
+      items,
+      navigate,
+      toggleDrawer,
+      isAuthenticated,
+      userName
+    };
+  },
+};
 </script>
+
+<style scoped>
+</style>
